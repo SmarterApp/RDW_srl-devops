a36831bb4c64439ead5160d3f26bda6e8f44d53e
Clinton Wolfe <clinton.wolfe@zenimaxonline.com>
2015-01-23 14:22:37 -0500

    Update spawner to use VPC to determine security groups and such

--- spawner/spawner.py
+++ spawner/spawner.py
@@ -8,13 +8,17 @@ import yaml
 import os
 from subprocess import Popen, PIPE
 import time
+from distutils.version import StrictVersion
+
+#import pdb; pdb.set_trace()
 
 def main():
     opts = read_cli_opts()
     cfg = read_config_file(opts)
-    ec2 = connect_to_ec2(opts, cfg)
-    hints = validate_request(opts, cfg, ec2)
-    spawn(hints, ec2)
+    ec2_conn = connect_to_ec2(opts, cfg)
+    vpc_conn = connect_to_vpc(opts, cfg)
+    hints = validate_request(opts, cfg, ec2_conn, vpc_conn)
+    spawn(hints, ec2_conn)
     logging.info('          ~ FIN ~')
     
 #=========================================================#
@@ -36,25 +40,33 @@ def get_aws_creds():
 
 def connect_to_ec2(opts, cfg):
     (access_id, secret_key) = get_aws_creds()
-    # Relies on reading creds from env vars
     conn = boto.ec2.connect_to_region(cfg['region'],
         aws_access_key_id=access_id,
         aws_secret_access_key=secret_key
     )
     logging.info("connected to AWS EC2 OK")
     return conn
-            
+
+def connect_to_vpc(opts, cfg):
+    (access_id, secret_key) = get_aws_creds()
+    vpc_conn = boto.vpc.VPCConnection(aws_access_key_id=access_id, aws_secret_access_key=secret_key)
+    logging.info("connected to AWS VPC OK")
+    return vpc_conn
+
 def read_cli_opts():
     parser = OptionParser()
-    parser.add_option("-c", "--count", type="int", dest="count", default=1,
-                        help="Number of instances to spawn", metavar="COUNT")
-    parser.add_option("-b", "--begin", type="int", dest="begin", default=1,
-                        help="Sequence number to begin at for naming", metavar="BEGIN")
     parser.add_option("-a", "--app", dest="app", type="string",
-                        help="Which app to spawn", metavar="APP")
+                        help="Which app to spawn.  Required; values are app names from spawner.yaml", metavar="APP")
+    parser.add_option("-b", "--begin", type="int", dest="begin", default=1,
+                        help="Sequence number to begin at for naming to avoid naming collisions.  Optional, default 1.", metavar="BEGIN")
+    parser.add_option("-c", "--count", type="int", dest="count", default=1,
+                        help="Number of instances to spawn.  Optional, default 1.", metavar="COUNT")
     parser.add_option("-d", "--debug", dest="debug", action="store_true",
                         help="Debug level output")
-    parser.add_option("-V", "--vpc", dest="vpc", metavar="VPC", type="string", default="dev", help="Which VPC to target")
+    parser.add_option("-e", "--env", dest="env", metavar="ENV", type="string",
+                       default="dev", help="Which environment (VPC) to target.  Defaults to dev; values are the Environment tags on the VPCs.")
+    parser.add_option("-V", "--app-version", dest="app_version", metavar="APP_VERSION", type="string",
+                       help="Which application version (AMI) to use.  AMI is selected by looking for an AMI tagged with the application and version.  Optional, latest version is default.")
     
     (options, args) = parser.parse_args()
      
@@ -108,7 +120,7 @@ def spawn(hints, ec2):
         instance.add_tags(hints['tags'])
         instance.add_tag('Name', value = hints['names'][idx])    
 
-def validate_request(opts, cfg, ec2):
+def validate_request(opts, cfg, ec2_conn, vpc_conn):
     logging.info("Gathering info about your spawn request....")
 
     hints = {}
@@ -128,7 +140,7 @@ def validate_request(opts, cfg, ec2):
     # TODO: consider examining DNS instead of / in addition to the AWS tags
     logging.debug("Generating names and checking for collisions")
     hints['names'] = ["srl-{0}-{1}".format(opts.app, str(i).zfill(3)) for i in range(opts.begin, opts.count + opts.begin)]
-    instance_tags = ec2.get_all_tags({'resource-type':'instance'})
+    instance_tags = ec2_conn.get_all_tags({'resource-type':'instance'})
     existing_names = [t.value for t in instance_tags if t.name == 'Name']
     for proposed_name in hints['names']:
         if proposed_name in existing_names:
@@ -138,46 +150,59 @@ def validate_request(opts, cfg, ec2):
     #------
     # Verify that AMI ID for that app type exists, and get tags
     #------
-    logging.debug("Fetching tags for AMI id {0}".format(app['ami']))
-    ami_tags = ec2.get_all_tags({'resource-type':'image','resource-id':app['ami']})
-    if not len(ami_tags) > 0:
-        logging.error("AMI id {0} either doesn't exist, or has no tags - you shall not pass.".format(app['ami']))
+    # TODO: rewrite to look for latest version
+    amis = ec2_conn.get_all_images(owners=['self'])
+    amis = [a for a in amis if a.tags.get('application') == opts.app]
+    if len(amis) < 1:
+        logging.error("No AMIs have the tag application:{0} - you shall not pass.".format(opts.app))
         exit(4)
-    hints['ami_id'] = app['ami']
+    if opts.app_version:
+        amis = [a for a in amis if a.tags.get('version') == opts.app_version]
+        if len(amis) < 1:
+            logging.error("No AMIs have the tags application:{0}, version:{1} - you shall not pass.".format(opts.app, opts.app_version))
+            exit(4)
+        hints['app_version'] = opts.app_version
+        ami = amis[0]
+    else:
+        amis.sort(key=lambda a: StrictVersion(a.tags.get('version')))
+        ami = amis[-1]
+        hints['app_version'] = ami.tags['version']
+    
+    hints['ami_id'] = ami.id
+
+    logging.info("Using version {0} of app {1} from AMI {2}".format(hints['app_version'], opts.app, hints['ami_id']))
+    
+    # Copy out tags for the AMI
     hints['tags'] = {}
     for tn in ['application', 'version', 'environment', 'tier']:
-        ami_tag = filter(lambda t: t.name == tn, ami_tags)
-        if len(ami_tag) > 0:
-            hints['tags'][tn] = ami_tag[0].value
-
-    #------
-    # Map security group names to IDs
-    #------
-    # TODO: Dev has no default VPC, so calling ec2.get_all_security_groups(groupnames=[...]) fails with a "No Default VPC" error.  BUt, oddly, we CAN lsit all SG's.
-    all_sgs = ec2.get_all_security_groups()
-    hints['security_group_ids'] = []
-    for sgn in app['security_groups']:
-        # TODO: There has got to be a be a better way
-        matched_sg = filter(lambda g: g.name == sgn, all_sgs)
-        if len(matched_sg) > 0:
-            hints['security_group_ids'].append(matched_sg[0].id)
-        else:
-            logging.error("Security group {0} does not seem to exist - you shall not pass.".format(sgn))
-            exit(5)
+       hints['tags'][tn] = ami.tags.get(tn)
 
     #------
     # Check VPC selection (via opts.vpc matching on VPC 'environment' tag)
     #------
     logging.debug("Verifying VPC selection")
-    (access_id, secret_key) = get_aws_creds()    
-    vpc_conn = boto.vpc.VPCConnection(aws_access_key_id=access_id, aws_secret_access_key=secret_key)
-    logging.info("connected to AWS VPC OK")
     vpcs = vpc_conn.get_all_vpcs()
-    selected_vpc = [v for v in vpcs if v.tags.get('Environment') == opts.vpc] # Note capitalization
+    selected_vpc = [v for v in vpcs if v.tags.get('Environment') == opts.env] # Note capitalization
     if len(selected_vpc) == 0 or len(selected_vpc) > 1:
-        logging.error("Could not find a unique VPC tagged with the {0} environment - you shall not pass.".format(opts.vpc))
+        logging.error("Could not find a unique VPC tagged with the {0} environment - you shall not pass.".format(opts.env))
         exit(6)
     hints['vpc_id'] = selected_vpc[0].id
+            
+    #------
+    # Map security group names to IDs
+    #------
+    # NOTE: Dev has no default VPC, so calling ec2_conn.get_all_security_groups(groupnames=[...]) fails with a "No Default VPC" error.  But, oddly, we CAN list all SG's.
+    sgs = ec2_conn.get_all_security_groups()
+    sgs = [sg for sg in sgs if sg.vpc_id == hints['vpc_id']]
+    hints['security_group_ids'] = []
+    for sgn in app['security_groups']:
+        matched_sg = [sg for sg in sgs if sg.name == sgn]
+        if len(matched_sg) == 1:
+            hints['security_group_ids'].append(matched_sg[0].id)
+        else:
+            logging.error("Could not find a unique security group {0} in vpc {1} - you shall not pass.".format(sgn, hints['vpc_id']))
+            exit(5)
+
         
     #------
     # Locate subnet within VPC using 'Tier' tag
@@ -195,7 +220,7 @@ def validate_request(opts, cfg, ec2):
     # TODO: Verify instance type ?
     hints['instance_type'] = app['instance_type']
     
-    logging.info("Preflight looks good.  Hang onto your hats!")
+    logging.info("Preflight looks good, you are cleared for launch attempt.")
     
     return hints
 
