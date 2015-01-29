b61b8863edd9c87813372f2e6066efec26f9beff
Clinton Wolfe <cwolfe@amplify.com>
2015-01-29 02:30:35 +0000

    Make dev vars more reusable, hopefully

--- ansible/filter_plugins/append_to_each.py
+++ ansible/filter_plugins/append_to_each.py
@@ -6,10 +6,17 @@ def append_to_each(arr, string):
 def instance_id_2_node_id(arg):
     return int(arg[2:], 16)
 
+def first(arg):
+    if len(arg) > 1:
+        return arg[0]
+    else:
+        return None
+
 class FilterModule(object):
     ''' Ansible custom jinja2 filters '''
     def filters(self):
         return {
             'append_to_each': append_to_each,
+            'first': first,
             'instance_id_2_node_id': instance_id_2_node_id,
         }
