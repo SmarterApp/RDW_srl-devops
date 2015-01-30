430197b8193f0b06dc5da3531fbfe56464b6773f
Clinton Wolfe <cwolfe@amplify.com>
2015-01-30 16:54:18 +0000

    Fix hostvars lookup - [ ] works on a different keyset than get(), apparently

--- ansible/filter_plugins/append_to_each.py
+++ ansible/filter_plugins/append_to_each.py
@@ -12,6 +12,18 @@ def first(arg):
     else:
         return None
 
+def srl_hostvars_lookup(hostvars, ip):
+    # ip might be None, or missing
+    # hostvars is a dict-like object that lies - keys() and get() operate on a different keyset than [ ]
+    # such hate
+    try:
+        info = hostvars[ip]
+        return info
+    except AnsibleError:
+        # OK, no host with that IP.
+        # Return a dummy hash.
+        return { 'ec2_tag_Name': 'dummy' }
+    
 class FilterModule(object):
     ''' Ansible custom jinja2 filters '''
     def filters(self):
@@ -19,4 +31,5 @@ class FilterModule(object):
             'append_to_each': append_to_each,
             'first': first,
             'instance_id_2_node_id': instance_id_2_node_id,
+            'srl_hostvars_lookup': srl_hostvars_lookup,
         }
