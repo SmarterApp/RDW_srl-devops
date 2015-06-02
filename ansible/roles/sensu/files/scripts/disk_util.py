40f6409cb1b559672db1f34abc0ef2942da186e9
Shmuel Goldstein <sgoldstein@amplify.com>
2015-06-02 00:22:02 +0000

    Fix: parse 1 line at a time (in progress)

--- ansible/roles/sensu/files/scripts/disk_util.py
+++ ansible/roles/sensu/files/scripts/disk_util.py
@@ -15,7 +15,7 @@ import time
 #files = ['avail_mb.wsp', 'capacity_pct.wsp', 'used_mb.wsp']
 files = ['used_mb.wsp']
 
-d = datetime.date(2015,5,3)
+d = datetime.date(2015,5,31)
 
 unix_epoch_may_1 = int(time.mktime(d.timetuple()))
 
@@ -24,30 +24,35 @@ grand_total = {}
 for dir in os.listdir("/data/carbon/whisper/sys"):
     print 'Getting', dir
 
-    path = '/data/carbon/whisper/sys/%s/disk/by_mount/_/' % dir
 
     for file in files:
-        proc = subprocess.Popen(["whisper-fetch --pretty --from %s %s%s | grep -v None" % (unix_epoch_may_1, path, file)], stdout=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
-        print "program output:", out
+        path = '/data/carbon/whisper/sys/%s/disk/by_mount/_/' % dir
+        cmd = ["whisper-fetch", "--pretty", "--from", "%s" % unix_epoch_may_1, "%s%s" % (path, file)]
 
+        proc = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+        while True:
+            line = proc.stdout.readline()
+            if line != '':
+            #the real code does filtering here
+                s = re.match("\S+ (\S+\s+[0-9]+) \S+ [0-9]+\s+([0-9]+)", line)
+                if not s:
+                    continue
+                totals = OrderedDict({})
+                month = s.group(1)
+                number = s.group(2)
+                #print month, number
 
-    s = re.findall("\S+ (\S+\s+[0-9]+) \S+ [0-9]+\s+([0-9]+)", out)
+                totals[month] = number
+            else:
+                break
 
-    totals = OrderedDict({})
+        print totals
+        for item in totals.items():
+            Day = item[0]
+            total = item[1]
+            print Day, total
+            if Day not in grand_total.keys():
+                grand_total[Day] = 0
 
-    for i in s:
-        month = i[0]
-        number = i[1]
-        totals[month] = number
-
-    print totals
-    for item in totals.items():
-        Day = item[0]
-        total = item[1]
-        print Day, total
-        if Day not in grand_total.keys():
-            grand_total[Day] = 0
-
-        grand_total[Day] += int(total)
+            grand_total[Day] += int(total)
     print 'Grand Totals:', grand_total
