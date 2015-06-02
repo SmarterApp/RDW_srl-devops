7d202b2b042ff7cfa58dc00533cc1f5bdfe7c666
Shmuel Goldstein <sgoldstein@amplify.com>
2015-06-02 19:19:38 +0000

    update

--- ansible/roles/sensu/files/scripts/disk_util.py
+++ ansible/roles/sensu/files/scripts/disk_util.py
@@ -13,46 +13,82 @@ import subprocess
 import time
 
 #files = ['avail_mb.wsp', 'capacity_pct.wsp', 'used_mb.wsp']
-files = ['used_mb.wsp']
+files = {"Used MB": 'used_mb.wsp', "Capacity": "capacity_pct.wsp"}
 
 d = datetime.date(2015,5,31)
 
-unix_epoch_may_1 = int(time.mktime(d.timetuple()))
+unix_epoch = int(time.mktime(d.timetuple()))
 
+
+mounts = {}
+mounts['bind'] = '_'
+mounts['file-trigger'] = '_'
+mounts['gluster-server'] = '_'
+mounts['lz'] = '_'
+mounts['migrate-db'] = '_mnt_data'
+mounts['reporting-generator-pdf'] = '_'
+mounts['reporting-db-pgpool'] = '_'
+mounts['reporting-db-slave'] = '_mnt_pgsql'
+mounts['reporting-rabbit-extract'] = '_'
+mounts['reporting-rabbit-services'] = '_'
+mounts['reporting-web'] = '_'
+mounts['reporting-worker-extract'] = '_'
+mounts['reporting-worker-pdf'] = '_'
+mounts['sensu'] = '_data'
+mounts['sensu-rabbit'] = '_'
+mounts['tsb-rabbit'] = '_'
+mounts['tsb-worker'] = '_'
+mounts['udl'] = '_'
+mounts['udl-db'] = '_mnt_data'
+mounts['udl-rabbit'] = '_'
 
 grand_total = {}
 for dir in os.listdir("/data/carbon/whisper/sys"):
-    print 'Getting', dir
 
+    print 'Starting', dir
 
-    for file in files:
-        path = '/data/carbon/whisper/sys/%s/disk/by_mount/_/' % dir
-        cmd = ["whisper-fetch", "--pretty", "--from", "%s" % unix_epoch_may_1, "%s%s" % (path, file)]
+    for title, file in files.iteritems():
+        found = False
+        for server_class in mounts:
+            if server_class + "-0" in dir:
+                mountpoint = mounts[server_class]
+                found = True
+                break
+        if not found:
+            raise SystemExit("define mountpoint for %s" % dir)
+        print 'Getting', dir, "mountpoint: ", mountpoint
+
+        path = '/data/carbon/whisper/sys/%s/disk/by_mount/%s/' % (dir, mountpoint)
+        cmd = ["whisper-fetch", "--pretty", "--from", "%s" % unix_epoch, "%s%s" % (path, file)]
+        print cmd
 
         proc = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+        totals = OrderedDict({})
+
         while True:
             line = proc.stdout.readline()
             if line != '':
-            #the real code does filtering here
-                s = re.match("\S+ (\S+\s+[0-9]+) \S+ [0-9]+\s+([0-9]+)", line)
+                s = re.match("\S+ (\S+)\s+[0-9]+ \S+ [0-9]+\s+([0-9]+)", line)
                 if not s:
                     continue
-                totals = OrderedDict({})
                 month = s.group(1)
                 number = s.group(2)
-                #print month, number
+                if number == "None":
+                    continue
 
                 totals[month] = number
             else:
                 break
 
-        print totals
         for item in totals.items():
             Day = item[0]
             total = item[1]
-            print Day, total
-            if Day not in grand_total.keys():
-                grand_total[Day] = 0
-
-            grand_total[Day] += int(total)
-    print 'Grand Totals:', grand_total
+            #print Day, total
+            if title not in grand_total.keys():
+                grand_total[title] = Day
+            
+            if title == "Used MB":
+                grand_total[title][Day] += int(total)
+            if title == "Capacity":
+                grand_total[title][Day] = (grand_total[title][Day] + int(total)) / 2
+        print '[%s] Grand Totals:' % title, grand_total
