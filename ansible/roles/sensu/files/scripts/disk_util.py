201586e4c00beb64eed9a5e57c0b71857cd2fe29
Shmuel Goldstein <sgoldstein@amplify.com>
2015-06-11 13:25:21 +0000

    Progress: display per-day disk-usage

--- ansible/roles/sensu/files/scripts/disk_util.py
+++ ansible/roles/sensu/files/scripts/disk_util.py
@@ -8,8 +8,11 @@
     this sensu server '''
 
 
+
+
 from collections import OrderedDict
 import datetime
+import locale
 import os
 import re
 import subprocess
@@ -19,9 +22,13 @@ import time
 #files = {"Used MB": 'used_mb.wsp', "Capacity": "capacity_pct.wsp"}
 #files = {"Used MB": 'used_mb.wsp'}
 
+locale.setlocale(locale.LC_ALL, 'en_US')
+
 metrics = {}
 metrics["Used MB"] = ['/data/carbon/whisper/sys/', '/disk/by_mount/', 'used_mb.wsp']
 metrics["Web-Response"] = ['/data/carbon/whisper/middleware/httpd/', '/response_usec/', 'percentile_90.wsp']
+
+print metrics
 
 d = datetime.date(2015,5,31)
 
@@ -29,35 +36,32 @@ unix_epoch = int(time.mktime(d.timetuple()))
 
 def fetch_whisper(wsp):
     cmd = ["whisper-fetch", "--pretty", "--from", "%s" % unix_epoch, wsp]
-    print cmd
     proc = subprocess.Popen(cmd,stdout=subprocess.PIPE)
-    totals = OrderedDict({})
+    totals = OrderedDict()
     
     while True:
         line = proc.stdout.readline()
         if line != '':
-            s = re.match("\S+ (\S+)\s+[0-9]+ \S+ [0-9]+\s+([0-9]+)", line)
+            s = re.match("\S+ (\S+)\s+([0-9]+) \S+ [0-9]+\s+([0-9]+)", line)
             if not s:
                 continue
             month = s.group(1)
-            number = s.group(2)
+            day = s.group(2)
+            number = s.group(3)
             if number == "None":
                 continue
+            number = int(number)
+            number = locale.format("%d", number, grouping=True)
     
-            totals[month] = number
+            if month not in totals:
+                totals[month] = OrderedDict()
+
+            totals[month][day] = number
         else:
             break
-    for item in totals.items():
-        Day = item[0]
-        total = item[1]
-                #print Day, total
-                #if title not in grand_total.keys():
-                #    grand_total[title] = Day
-        if Day not in grand_total.keys():
-            grand_total[Day] = 0
-                
-        grand_total[Day] += int(total)
-        print "Grant total so far:", grand_total
+    for month, days in totals.iteritems():
+        for day, total in days.iteritems():
+            print month, day, total
 
 def loop_mounts(app, path, metric_file):
     mounts = []
