1f09b6cdef61a93a35b2c0961c7afe80c10b4cae
Shmuel Goldstein <sgoldstein@amplify.com>
2015-06-11 00:20:43 +0000

    Progress: refactor script for all 3 metrics: disk-stats [done], web-response [in-progress]

--- ansible/roles/sensu/files/scripts/disk_util.py
+++ ansible/roles/sensu/files/scripts/disk_util.py
@@ -1,7 +1,10 @@
 #!/bin/env python
 
 ''' This script will display a grand total of
-    disk usage for all servers monitored on
+      - disk usage 
+      - web-response
+      - concurrent sessions
+    for all servers monitored on
     this sensu server '''
 
 
@@ -13,59 +16,72 @@ import subprocess
 import time
 
 #files = ['avail_mb.wsp', 'capacity_pct.wsp', 'used_mb.wsp']
-files = {"Used MB": 'used_mb.wsp', "Capacity": "capacity_pct.wsp"}
-files = {"Used MB": 'used_mb.wsp'}
+#files = {"Used MB": 'used_mb.wsp', "Capacity": "capacity_pct.wsp"}
+#files = {"Used MB": 'used_mb.wsp'}
+
+metrics = {"Used MB": ['/data/carbon/whisper/sys/', '/disk/by_mount/', 'used_mb.wsp']}
+files = {"Web-Response": ['/data/carbon/whisper/middleware/httpd/', '/response_usec/', 'percentile_90.wsp']}
 
 d = datetime.date(2015,5,31)
 
 unix_epoch = int(time.mktime(d.timetuple()))
 
 grand_total = {}
-for app in os.listdir("/data/carbon/whisper/sys"):
 
-    for title, file in files.iteritems():
-        total_dirs = 0
-        mounts = []
-        for mountpoint in os.listdir("/data/carbon/whisper/sys/" + app + "/disk/by_mount/"):
-            total_dirs += 1
-            mounts.append(mountpoint)
-        if len(mounts) > 1:
-            mounts.remove("_")
+def fetch_whisper(wsp):
+    cmd = ["whisper-fetch", "--pretty", "--from", "%s" % unix_epoch, wsp]
+    proc = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+    totals = OrderedDict({})
+    
+    while True:
+        line = proc.stdout.readline()
+        if line != '':
+            s = re.match("\S+ (\S+)\s+[0-9]+ \S+ [0-9]+\s+([0-9]+)", line)
+            if not s:
+                continue
+            month = s.group(1)
+            number = s.group(2)
+            if number == "None":
+                continue
+    
+            totals[month] = number
+        else:
+            break
+    for item in totals.items():
+        Day = item[0]
+        total = item[1]
+                #print Day, total
+                #if title not in grand_total.keys():
+                #    grand_total[title] = Day
+        if Day not in grand_total.keys():
+            grand_total[Day] = 0
+                
+        grand_total[Day] += int(total)
+        print "Grant total so far:", grand_total
 
-        for mountpoint in mounts:
+def loop_mounts(app, path, metric_file):
+    mounts = []
+    total_dirs = 0
+    for mountpoint in os.listdir(path):
+        total_dirs += 1
+        mounts.append(mountpoint)
+    if len(mounts) > 1:
+        mounts.remove("_")
+    
+    for mountpoint in mounts:
+        mountpointS = mountpoint.replace("_","/")
+        print '\n\nGetting', app, "mountpoint: ", mountpointS
+    wsp = path + mountpoint + "/" + metric_file
+    fetch_whisper(wsp)
 
-            mountpointS = mountpoint.replace("_","/")
-            print '\n\nGetting', app, "mountpoint: ", mountpointS
-
-            path = '/data/carbon/whisper/sys/%s/disk/by_mount/%s/' % (app, mountpoint)
-            cmd = ["whisper-fetch", "--pretty", "--from", "%s" % unix_epoch, "%s%s" % (path, file)]
-
-            proc = subprocess.Popen(cmd,stdout=subprocess.PIPE)
-            totals = OrderedDict({})
-
-            while True:
-                line = proc.stdout.readline()
-                if line != '':
-                    s = re.match("\S+ (\S+)\s+[0-9]+ \S+ [0-9]+\s+([0-9]+)", line)
-                    if not s:
-                        continue
-                    month = s.group(1)
-                    number = s.group(2)
-                    if number == "None":
-                        continue
-
-                    totals[month] = number
-                else:
-                    break
-
-            for item in totals.items():
-                Day = item[0]
-                total = item[1]
-            #print Day, total
-            #if title not in grand_total.keys():
-            #    grand_total[title] = Day
-                if Day not in grand_total.keys():
-                    grand_total[Day] = 0
-            
-                grand_total[Day] += + int(total)
+for metric in metrics:
+    path_prefix = metrics[metric][0]
+    path_suffix = metrics[metric][1]
+    metric_file = metrics[metric][2]
+    for app in os.listdir(path_prefix):
+        path = path_prefix + app + path_suffix
+    
+        for title, file in files.iteritems():
+            if metric == "Used MB":
+                loop_mounts(app, path, metric_file)
 print '[%s] Grand Totals:' % title, grand_total
