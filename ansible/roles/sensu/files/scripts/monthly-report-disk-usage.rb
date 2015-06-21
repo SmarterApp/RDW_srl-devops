fec01cfbf276ed26b450e8559dc64d5f6cda0ff3
Clinton Wolfe <cwolfe@amplify.com>
2015-06-21 20:19:59 +0000

    Refactor to share whisper access code

--- ansible/roles/sensu/files/scripts/monthly-report-disk-usage.rb
+++ ansible/roles/sensu/files/scripts/monthly-report-disk-usage.rb
@@ -3,22 +3,15 @@
 # This script will create a csv file with the daily max of disk usage
 #    for servers monitored on this sensu server '''
 
-require 'date'
-require 'csv'
+require_relative './monthly-report.rb'
 
-class DiskUsageReport
+class DiskUsageReport < MonthlyReport
 
-  WHISPER_PATH='/opt/carbon/whisper/'
-  CSV_PATH='/tmp/'
-  
   def initialize
+    super
     # by day, by server type, by server-mount = max usage over the day
     @usage = {}
 
-    # TODO _ read these from CLI
-    @from_sec_ago = 30 * 60*60*24
-    @until_sec_ago = 0
-    
     @glob_for_app_class = {
       :gluster => 'sys/srl-gluster-server-???/disk/by_mount/*/used_mb.wsp',
       # On database servers, we only want mounts that are the data dir - which varies :(
@@ -69,29 +62,6 @@ class DiskUsageReport
 
     end
   end
-  
-  def whisper_fetch(filename)
-    epoch_start = Time.now.to_i - @from_sec_ago
-    epoch_end = Time.now.to_i - @until_sec_ago
-    `whisper-fetch --from #{epoch_start} --until #{epoch_end} #{filename} | grep -v None`.each_line do |line|
-      line.chomp!
-      next if line.empty?
-      match = /(\d+)\s+(\d+\.?\d*)/.match(line)
-      next unless match
-      epoch = match[1]
-      value = match[2].to_f
-      date = DateTime.strptime(epoch, '%s').strftime('%F')
-      yield({epoch: epoch, value: value, date: date})      
-    end
-  end
-  
-  def list_whisper_files(glob)
-    # stat filename and filter out any that have not been updated in our window of interest
-    return Dir.glob(WHISPER_PATH + glob).reject do |filename|
-      File::Stat.new(filename).mtime.to_i < (Time.now.to_i - @from_sec_ago)
-    end.sort
-  end
-  
 end
 
 DiskUsageReport.new().run()
