507b5bb5083a1815cea16f12c37df980eecc3e38
Clinton Wolfe <cwolfe@amplify.com>
2015-07-22 18:50:38 +0000

    Re-arrange service stop/start playbook generators

--- ansible/make-shutdown-playbook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -f stop-all-services.yml
-for APP in $(cat roles/orchestration/files/shutdown-sequence.txt | fgrep -v '#'); do
-    sed stop-all-services.yml.template -e "s/APPNAME/$APP/g" >> stop-all-services.yml
-done
